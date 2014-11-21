7df47b0facee88547cc1d8c80fe3c447f3070573
synergycns <synergycns@users.noreply.github.com>
2014-11-21 15:47:56 -0600

    Update default.rb

--- php-phalcon/attributes/default.rb
+++ php-phalcon/attributes/default.rb
@@ -1,3 +1,6 @@
 default['php-phalcon']['git_url'] = 'git://github.com/phalcon/cphalcon.git'
 default['php-phalcon']['git_ref'] = 'master'
+default['php-phalcon']['packages'] = ['php-devel', 'pcre-devel', 'gcc', 'make']
+default['php-phalcon']['conf_dirs'] = ['/etc/php.d']
+default['php-phalcon']['conf_file'] = 'phalcon.ini'
 default['php-phalcon']['devtools'] = true
