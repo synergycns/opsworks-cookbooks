be77881c3b03710859788757a175435c31cd0a23
synergycns <synergycns@users.noreply.github.com>
2015-01-21 06:18:14 -0600

    Update default.rb

--- ioncube/recipes/default.rb
+++ ioncube/recipes/default.rb
@@ -5,11 +5,11 @@ bash "ioncube" do
   code <<-EOH
   (cd /tmp; wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.bz2)
   (cd /tmp; tar jxvf ioncube_loaders_lin_x86-64.tar.bz2)
-  (cd /tmp/ioncube; mv ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/ioncube_loader.so)
-  (chown root:root /usr/lib/php5/20121212/ioncube_loader.so)
+  (cd /tmp/ioncube; mv ioncube_loader_lin_5.5.so /usr/lib64/php/modules/ioncube_loader.so)
+  (chown root:root /usr/lib64/php/modules/ioncube_loader.so)
   EOH
 end
 
-template "/etc/php5/apache2/conf.d/00-ioncube.ini" do
+template "/etc/php.d/ioncube.ini" do
   source 'ioncube.ini.erb'
 end
