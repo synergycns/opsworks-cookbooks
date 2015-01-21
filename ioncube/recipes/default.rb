bd143447f04dc9ab6e03d27e3cb34e5165d5d059
synergycns <synergycns@users.noreply.github.com>
2015-01-21 06:33:14 -0600

    Update default.rb

--- ioncube/recipes/default.rb
+++ ioncube/recipes/default.rb
@@ -5,7 +5,7 @@ bash "ioncube" do
   code <<-EOH
   (cd /tmp; wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.bz2)
   (cd /tmp; tar jxvf ioncube_loaders_lin_x86-64.tar.bz2)
-  (cd /tmp/ioncube; mv ioncube_loader_lin_5.5.so /usr/lib64/php/modules/ioncube_loader.so)
+  (cd /tmp/ioncube; mv ioncube_loader_lin_5.3.so /usr/lib64/php/modules/ioncube_loader.so)
   (chown root:root /usr/lib64/php/modules/ioncube_loader.so)
   EOH
 end
