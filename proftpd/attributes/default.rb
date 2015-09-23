29ad5746b90d3c52edbdb918944c1ca28c509565
synergycns <synergycns@users.noreply.github.com>
2015-09-22 22:06:10 -0500

    Update default.rb

--- proftpd/attributes/default.rb
+++ proftpd/attributes/default.rb
@@ -1,10 +1,9 @@
 # Default configuration for the AWS OpsWorks cookbook for proftpd
 #
 
-layer               = node[:opsworks][:instance][:layers].first
-instance            = node[:opsworks][:layers].fetch(layer)[:instances].first[1]
+instance            = node[:opsworks][:instance]
 instance_dns_name   = instance[:public_dns_name]
-instance_elastic_ip = instance[:elastic_ip]
+instance_ip         = instance[:ip]
 
 
 default[:proftpd] = {
@@ -23,6 +22,6 @@ default[:proftpd] = {
   :dbhost => "localhost",
   :folder_path => "/srv/www",
   :passive_ports => "1024 1048",
-  :elastic_ip => instance_elastic_ip,
+  :elastic_ip => instance_ip,
   :server_name => instance_dns_name
 }
